92bbac94a0ff9ff28414853174f51e78b6791871
13240 <13240@DESKTOP-7JI1B9R>
2018-05-23 18:50:20 +0800

    初始化

---
+++ ironMan-web/src/main/java/com/fighttiger/fighter/ironman/modules/domain/base/IDomain.java
@@ -0,0 +1,12 @@
+package com.fighttiger.fighter.ironman.modules.domain.base;
+
+import java.io.Serializable;
+
+/**
+ * domain实体接口
+ * @author weiqz
+ *
+ */
+public interface IDomain extends Serializable {
+
+}
